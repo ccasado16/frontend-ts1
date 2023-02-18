080a12ae7b8129d1d3df3650f42d682506530c24
Carlos Casado Vargas <carloscasadovargas@gmail.com>
2023-02-18 12:12:32 -0500

    hora formateada

--- src/components/Admin/TableDetails/OrderItemAdmin/OrderItemAdmin.js
+++ src/components/Admin/TableDetails/OrderItemAdmin/OrderItemAdmin.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Button, Image } from "semantic-ui-react";
 import classNames from "classnames";
+import moment from "moment";
+import "moment/locale/es";
 import "./OrderItemAdmin.scss";
 
 export function OrderItemAdmin(props) {
@@ -13,7 +15,10 @@ export function OrderItemAdmin(props) {
         [order.status.toLowerCase()]: true,
       })}
     >
-      <div className="order-item-admin__time">{order.created_at}</div>
+      <div className="order-item-admin__time">
+        <span>{moment(order.created_at).format("HH:mm")}</span> {" - "}
+        <span>{moment(order.created_at).startOf("seconds").fromNow()}</span>
+      </div>
       <div className="order-item-admin__product">
         <Image src={image} />
         <p>{title}</p>
