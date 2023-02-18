c5b407f28faf33aa2566fade48cddfbdab34ef5c
Carlos Casado Vargas <carloscasadovargas@gmail.com>
2023-02-18 11:53:29 -0500

    hook indexed

--- src/pages/Admin/TableDetailsAdmin.js
+++ src/pages/Admin/TableDetailsAdmin.js
@@ -1,5 +1,29 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { Loader } from "semantic-ui-react";
+import { useParams } from "react-router-dom";
+import { useOrder } from "../../hooks";
+import { HeaderPage } from "../../components/Admin";
+import { ListOrderAdmin } from "../../components/Admin/TableDetails";
 
 export function TableDetailsAdmin() {
-  return <div>TableDetailsAdmin</div>;
+  const { id } = useParams();
+  const { loading, orders, getOrdersByTable } = useOrder();
+
+  useEffect(() => {
+    getOrdersByTable(id);
+  }, []);
+
+  return (
+    <>
+      <HeaderPage title={`Mesa ***`} />
+
+      {loading ? (
+        <Loader active inline="centered">
+          Cargando...
+        </Loader>
+      ) : (
+        <ListOrderAdmin orders={orders} />
+      )}
+    </>
+  );
 }
