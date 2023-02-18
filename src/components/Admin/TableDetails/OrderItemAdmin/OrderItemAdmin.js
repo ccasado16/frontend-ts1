3ebdfc036f7884c2ae14cdbccd6cae9b6fd9c198
Carlos Casado Vargas <carloscasadovargas@gmail.com>
2023-02-18 12:01:57 -0500

    order item coloreado depende del estado

--- src/components/Admin/TableDetails/OrderItemAdmin/OrderItemAdmin.js
+++ src/components/Admin/TableDetails/OrderItemAdmin/OrderItemAdmin.js
@@ -1,12 +1,18 @@
 import React from "react";
 import { Button, Image } from "semantic-ui-react";
+import classNames from "classnames";
 import "./OrderItemAdmin.scss";
 
 export function OrderItemAdmin(props) {
   const { order } = props;
   const { title, image } = order.product_data;
+  console.log(order);
   return (
-    <div className="order-item-admin">
+    <div
+      className={classNames("order-item-admin", {
+        [order.status.toLowerCase()]: true,
+      })}
+    >
       <div className="order-item-admin__time">{order.created_at}</div>
       <div className="order-item-admin__product">
         <Image src={image} />
