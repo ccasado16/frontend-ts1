81a2b8afd14fd364b6230e2d89246492a9e9cd3b
Carlos Casado Vargas <carloscasadovargas@gmail.com>
2023-03-01 15:18:54 -0500

    vista de carrito vacio

--- src/pages/Client/Cart.js
+++ src/pages/Client/Cart.js
@@ -1,11 +1,15 @@
 import React, { useEffect, useState } from "react";
+import { Button } from "semantic-ui-react";
+import { Link, useParams } from "react-router-dom";
 import { useProduct } from "../../hooks";
 import { getProductsCart } from "../../api/cart";
+import { size } from "lodash";
 
 export function Cart() {
   const [products, setProducts] = useState(null);
   const { getProductById } = useProduct();
-  console.log(products);
+  const { tableNumber } = useParams();
+
   useEffect(() => {
     (async () => {
       const idProductsCart = getProductsCart();
@@ -20,5 +24,21 @@ export function Cart() {
     })();
   }, []);
 
-  return <div>Cart</div>;
+  return (
+    <div>
+      <h3>Carrito</h3>
+      {!products ? (
+        <p>Cargando ..</p>
+      ) : size(products) === 0 ? (
+        <div style={{ textAlign: "center" }}>
+          <p>Tu carrito está vacío</p>
+          <Link to={`/client/${tableNumber}/orders`}>
+            <Button primary>Ver pedidos</Button>
+          </Link>
+        </div>
+      ) : (
+        <p>Lista de productos en el carrito</p>
+      )}
+    </div>
+  );
 }
