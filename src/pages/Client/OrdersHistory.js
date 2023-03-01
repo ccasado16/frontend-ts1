a1babc812deb18f32bf8835986fe2a31e7ff15d5
Carlos Casado Vargas <carloscasadovargas@gmail.com>
2023-03-01 16:37:00 -0500

    obteniendo pedidos de la mesa

--- src/pages/Client/OrdersHistory.js
+++ src/pages/Client/OrdersHistory.js
@@ -1,6 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { useOrder, useTable } from "../../hooks";
 
 export function OrdersHistory() {
+  const { loading, orders, getOrdersByTable } = useOrder();
+  const { getTableByNumber } = useTable();
+  const { tableNumber } = useParams();
+  console.log(orders);
+
+  useEffect(() => {
+    (async () => {
+      const table = await getTableByNumber(tableNumber);
+      const idTable = table[0].id;
+
+      getOrdersByTable(idTable, "", "ordering=-status,-created_at");
+    })();
+  }, []);
+
   return (
     <div>
       <h3>Historial de pedidos</h3>
