3ecce7d54325d9d693d95cc0096043ceb829e3be
Carlos Casado Vargas <carloscasadovargas@gmail.com>
2023-03-01 14:47:32 -0500

    obteniendo productos del carrito

--- src/pages/Client/Cart.js
+++ src/pages/Client/Cart.js
@@ -1,5 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useProduct } from "../../hooks";
+import { getProductsCart } from "../../api/cart";
 
 export function Cart() {
+  const [products, setProducts] = useState(null);
+  const { getProductById } = useProduct();
+  console.log(products);
+  useEffect(() => {
+    (async () => {
+      const idProductsCart = getProductsCart();
+
+      const productsArray = [];
+
+      for await (const idProduct of idProductsCart) {
+        const response = await getProductById(idProduct);
+        productsArray.push(response);
+      }
+      setProducts(productsArray);
+    })();
+  }, []);
+
   return <div>Cart</div>;
 }
